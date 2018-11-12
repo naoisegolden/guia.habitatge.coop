8a509eb5e5dbfeece48561f2f7c2fbb8617e612d
Naoise Golden <naoise.golden@gmail.com>
2018-11-12 21:29:31 +0100

    404 page

--- wp-content/themes/laborda/404.php
+++ wp-content/themes/laborda/404.php
@@ -20,36 +20,9 @@ get_header();
 					</header><!-- .page-header -->
 
 					<div class="page-content">
-						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'laborda' ); ?></p>
+						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'laborda' ); ?></p>
 
-						<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-						?>
-
-						<div class="widget widget_categories">
-							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'laborda' ); ?></h2>
-							<ul>
-								<?php
-								wp_list_categories( array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								) );
-								?>
-							</ul>
-						</div><!-- .widget -->
-
-						<?php
-						/* translators: %1$s: smiley */
-						$laborda_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'laborda' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$laborda_archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-						?>
+						<?php get_search_form(); ?>
 
 					</div><!-- .page-content -->
 				</section><!-- .error-404 -->
@@ -58,6 +31,13 @@ get_header();
 		</div><!-- #primary -->
 	</div><!-- .row -->
 </div><!-- .container -->
+
+
+<div class="container-jumbo">
+	<div class="container container-content">
+		<?php get_template_part( 'template-parts/content', 'jumbo-help' ); ?>
+	</div><!-- .container -->
+</div><!-- .container-jumbo -->
 
 <?php
 get_footer();
---
+++ wp-content/themes/laborda/template-parts/content-jumbo-help.php
@@ -0,0 +1,11 @@
+<div class="row">
+  <div class="col-4 offset-1">
+    <img src="https://via.placeholder.com/350x450" alt="placeholder">
+  </div>
+  <div class="col-5 offset-1">
+    <h4><?php echo __( 'No trobes el que busques? Prova', 'laborda' ) ?></h4>
+    <a class="btn btn-primary mt-2" href="<?php echo get_permalink( get_page_by_path( 'help' ) ) ?>" role="button">
+      <?php echo __( 'Per on començar', 'laborda' ); ?>
+    </a>
+  </div>
+</div>
