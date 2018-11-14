b638dfbdcd1b1e9151d3e5b195ef39c0f6991c21
Naoise Golden <naoise.golden@gmail.com>
2018-11-14 22:44:12 +0100

    Better design for search form

--- wp-content/themes/laborda/searchform.php
+++ wp-content/themes/laborda/searchform.php
@@ -1,9 +1,10 @@
-<form class="form-inline search-form row" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-  <div class="form-group col-9">
-  	<label for="search">
-  		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'laborda' ); ?></span>
-  	</label>
-  	<input type="search" class="form-control" id="search" placeholder="<?php echo esc_attr_x( 'Search…', 'laborda' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-  </div><!-- .form-group -->
-  <button type="submit" class="btn btn-primary col-3"><?php echo _x( 'Search', 'laborda' ); ?></button>
-</form>
+<div class="row">
+  <div class="col-12">
+    <form class="input-group search-form" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+      <input type="text" class="form-control" id="search" aria-label="Search" placeholder="<?php echo esc_attr_x( 'Search…', 'laborda' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+      <div class="input-group-append">
+        <button type="submit" class="btn btn-primary"><?php echo _x( 'Search', 'laborda' ); ?></button>
+      </div>
+    </form>
+  </div>
+</div>
