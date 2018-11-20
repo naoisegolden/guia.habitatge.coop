691e44b9c24f8fa4cdf5f680bafbc6f5829de288
Naoise Golden <naoise.golden@gmail.com>
2018-11-20 21:16:55 +0100

    Adjust width 'more info' section

--- wp-content/themes/laborda/template-parts/content-activity-more-info.php
+++ wp-content/themes/laborda/template-parts/content-activity-more-info.php
@@ -1,5 +1,5 @@
 <div class="row">
-  <div class="col">
+  <div class="col-8 offset-2">
     <h2><?php echo __('Més informació', 'laborda'); ?></h2>
     <?php the_field( 'more_information' ); ?>
   </div>
