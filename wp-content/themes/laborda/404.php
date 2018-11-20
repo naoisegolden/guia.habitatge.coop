c0cd2ca3f9a69cffaf00f4df5d86226ca043d8fa
Naoise Golden <naoise.golden@gmail.com>
2018-11-20 22:15:00 +0100

    404 page

--- wp-content/themes/laborda/404.php
+++ wp-content/themes/laborda/404.php
@@ -11,7 +11,7 @@ get_header();
 ?>
 <div class="container">
 	<div class="row">
-		<div id="primary" class="content-area col">
+		<div id="primary" class="content-area col-8 offset-2">
 			<main id="main" class="site-main">
 
 				<section class="error-404 not-found">
@@ -19,7 +19,7 @@ get_header();
 						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'laborda' ); ?></h1>
 					</header><!-- .page-header -->
 
-					<div class="page-content">
+					<div class="page-content mb-5">
 						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'laborda' ); ?></p>
 
 						<?php get_search_form(); ?>
@@ -31,13 +31,6 @@ get_header();
 		</div><!-- #primary -->
 	</div><!-- .row -->
 </div><!-- .container -->
-
-
-<div class="container-jumbo">
-	<div class="container container-content">
-		<?php get_template_part( 'template-parts/content', 'jumbo-help' ); ?>
-	</div><!-- .container -->
-</div><!-- .container-jumbo -->
 
 <?php
 get_footer();
