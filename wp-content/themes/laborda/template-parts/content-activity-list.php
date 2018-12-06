d367815eac1c069b0e8e976af581578325af9853
Naoise Golden <naoise.golden@gmail.com>
2018-12-06 21:46:38 +0100

    Option to hide activity page

--- wp-content/themes/laborda/template-parts/content-activity-list.php
+++ wp-content/themes/laborda/template-parts/content-activity-list.php
@@ -5,6 +5,7 @@ $groups = [$categories[0]->slug];
 if ( $phases[0] ) $groups[] = $phases[0]->slug;
 $classes = 'activity-card activity-card__' . $categories[0]->slug;
 $view_count = function_exists('pvc_get_post_views') ? pvc_get_post_views() : 0;
+$hide_page = get_field( 'hide_activity_page' );
 ?>
 
 <div class="col-6 col-md-4 col-lg-3 js-card"
@@ -15,7 +16,11 @@ $view_count = function_exists('pvc_get_post_views') ? pvc_get_post_views() : 0;
 	data-views="<?php echo $view_count; ?>"
 >
 	<article id="activity-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
-		<a class="activity-card__body" href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+		<?php if ( $hide_page ) : ?>
+			<div class="activity-card__body">
+		<?php else : ?>
+			<a class="activity-card__body" href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+		<?php endif; ?>
 			<?php the_title( '<h2>', '</h2>' ); ?>
 			<p><?php echo get_the_excerpt(); ?></p>
 			<?php
@@ -24,6 +29,11 @@ $view_count = function_exists('pvc_get_post_views') ? pvc_get_post_views() : 0;
 				'after'  => '</div>',
 			) );
 			?>
-		</a>
+		<?php if ( $hide_page ) : ?>
+			</div>
+		<?php else : ?>
+			</a>
+		<?php endif; ?>
+
 	</article><!-- #post-<?php the_ID(); ?> -->
 </div>
