cd3cff63c9d2826a24ba6d06946bb5ac43da73ea
Naoise Golden <naoise.golden@gmail.com>
2019-01-05 17:45:26 +0100

    Option to hide title in pages

--- wp-content/themes/laborda/template-parts/content-page.php
+++ wp-content/themes/laborda/template-parts/content-page.php
@@ -10,9 +10,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<?php if ( !get_field( 'hide_title' ) ) : ?>
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<?php laborda_post_thumbnail(); ?>
 
