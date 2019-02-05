0747866a5dc13986008b21a52ac50a3a70061c0d
Naoise Golden <naoise.golden@gmail.com>
2019-02-05 21:08:21 +0100

    Wider button for search

--- wp-content/themes/laborda/searchform.php
+++ wp-content/themes/laborda/searchform.php
@@ -3,7 +3,7 @@
     <form class="input-group search-form" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
       <input type="text" class="form-control" id="search" aria-label="Search" placeholder="<?php echo esc_attr_x( 'Search…', 'placeholder', 'laborda' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
       <div class="input-group-append">
-        <button type="submit" class="btn btn-primary"><?php echo _x( 'Search', 'submit button', 'laborda' ); ?></button>
+        <button type="submit" class="btn btn-primary btn-wide-md"><?php echo _x( 'Search', 'submit button', 'laborda' ); ?></button>
       </div>
     </form>
   </div>
