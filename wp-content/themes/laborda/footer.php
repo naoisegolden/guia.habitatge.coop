00b4bce8ed5a33ad7a4aa22ca96b3e7ea6d24aee
Naoise Golden <naoise.golden@gmail.com>
2019-02-05 21:43:24 +0100

    Remove footer logo and description

--- wp-content/themes/laborda/footer.php
+++ wp-content/themes/laborda/footer.php
@@ -26,11 +26,6 @@
 	<?php endif; ?>
 	<footer id="colophon" class="site-footer">
 		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<p class="site-description"><?php echo get_bloginfo( 'description', 'display' ); ?></p>
-				</div>
-			</div>
 			<div class="row text-center text-md-left">
 				<div class="col-12 col-md-8">
 					<?php dynamic_sidebar( 'footer_left' ); ?>
